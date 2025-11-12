375b897da8db3875e62863365b28e8561f18d5b3
25021561 <hatsunekaiyoisgay2222@gmail.com>
2025-11-12 17:01:11 +0700

    commit message template

--- Du an.py
+++ Du an.py
@@ -1,13 +1,1 @@
-#step 1: Nhập tuổi của bạn
-a = input("Bạn bao nhiêu tuổi? ")
-a = int(a)
-#step 2: Nhập tên của bạn
-b = input("Tên của bạn là gì? ")
-
-#step 3: In ra câu chào hỏi
-if a < 18:
-     print("Xin chào " + b + ", xét theo tuổi của bạn hiện tại là " + str(a) + ", thì có vẻ như bạn vẫn chưa đủ tuổi vị thành niên.")
-elif a == 18:
-     print("Xin chào " + b + ", xét theo tuổi của bạn hiện tại là " + str(a) + ", thì có vẻ như bạn vừa mới đủ tuổi vị thành niên.")
-else:
-     print("Xin chào " + b + ", xét theo tuổi của bạn hiện tại là " + str(a) + ", thì có vẻ như bạn đã đủ tuổi vị thành niên.")
+print("Hello World")                            
---
+++ thử.py
@@ -0,0 +1,4 @@
+import random
+def Sigma(a, b):
+    return random.choice([a, b])
+print("The sigma is", Sigma("Minh", "Long"))
